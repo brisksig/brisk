9379e046a6dc4518de360d14e2d04b21db25cc7f
DomineCore <64761294@qq.com>
2022-04-01 19:34:56 +0800

    minor: .go文件添加开源协议文件头

--- types.go
+++ types.go
@@ -1,3 +1,6 @@
+// Copyright 2022 DomineCore.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
 package brisk
 
 type HandleFunc func(c *Context)
